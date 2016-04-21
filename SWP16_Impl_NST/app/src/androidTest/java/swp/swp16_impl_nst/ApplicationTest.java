c7ab38225df0b6fdc9df5fc1e0852ff286685545
pflingstring <nzaporojan@gmail.com>
2016-04-21 13:17:26 +0200

    create empty android project

---
+++ SWP16_Impl_NST/app/src/androidTest/java/swp/swp16_impl_nst/ApplicationTest.java
@@ -0,0 +1,15 @@
+package swp.swp16_impl_nst;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application>
+{
+    public ApplicationTest()
+    {
+        super(Application.class);
+    }
+}
