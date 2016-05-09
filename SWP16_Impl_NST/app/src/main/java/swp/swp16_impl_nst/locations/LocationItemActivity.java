826d2966f4e68fa01d28b084166c5ba13a61fb2d
pflingstring <nzaporojan@gmail.com>
2016-05-09 19:18:20 +0200

    add edit button to AppBar in LocationItemActivity

--- SWP16_Impl_NST/app/src/main/java/swp/swp16_impl_nst/locations/LocationItemActivity.java
+++ SWP16_Impl_NST/app/src/main/java/swp/swp16_impl_nst/locations/LocationItemActivity.java
@@ -2,7 +2,10 @@ package swp.swp16_impl_nst.locations;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import swp.swp16_impl_nst.R;
 import swp.swp16_impl_nst.models.locations.Location;
@@ -39,6 +42,28 @@ public class LocationItemActivity extends AppCompatActivity
             comment.setText(location.getComment());
             address.setText(location.getAddress().toString());
         }
+    }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_location_item, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        switch (item.getItemId())
+        {
+            case R.id.edit_location :
+                Toast toast = Toast.makeText(getApplicationContext(), "Edit item " + item.getItemId(), Toast.LENGTH_SHORT);
+                toast.show();
+
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
