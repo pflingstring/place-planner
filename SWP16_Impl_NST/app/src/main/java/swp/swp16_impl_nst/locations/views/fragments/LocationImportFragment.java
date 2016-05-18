11a5b85ed62731725c629421dc482db8864a6fb4
Simon <rommelsp@students.uni-marburg.de>
2016-05-18 20:09:22 +0200

    new Branch für Import-Umsetzung

---
+++ SWP16_Impl_NST/app/src/main/java/swp/swp16_impl_nst/locations/views/fragments/LocationImportFragment.java
@@ -0,0 +1,26 @@
+package swp.swp16_impl_nst.locations.views.fragments;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import swp.swp16_impl_nst.R;
+
+/**
+ * Created by Simon on 18.05.2016.
+ */
+public class LocationImportFragment extends Fragment{
+
+    public LocationImportFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_location_import, container, false);
+
+        return rootView;
+    }
+}
