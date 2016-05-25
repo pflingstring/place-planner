207b895a06fa7a2eaa8dcbe3c081f544c52e393a
Simon <rommelsp@students.uni-marburg.de>
2016-05-25 20:34:20 +0200

    neues Klassendiagramm hinzugefügt, Import/Export vorbereitet, SearchButton hinzugefügt

--- SWP16_Impl_NST/app/src/main/java/swp/swp16_impl_nst/locations/views/LocationImportActivity.java
+++ SWP16_Impl_NST/app/src/main/java/swp/swp16_impl_nst/locations/views/LocationImportActivity.java
@@ -4,7 +4,9 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
-
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import swp.swp16_impl_nst.R;
 
@@ -14,17 +16,44 @@ import swp.swp16_impl_nst.R;
  */
 public class LocationImportActivity extends AppCompatActivity{
 
+    private Button importButton;
+    private Button cancelButton;
+    private Button durchsuchenButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_location_import);
+
+        importButton = (Button) findViewById(R.id.importButton);
+        cancelButton = (Button) findViewById(R.id.cancelButton);
+        durchsuchenButton = (Button) findViewById(R.id.durchsuchenButton);
+
+        importButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Toast toast = Toast.makeText(getApplicationContext(), "Import Button pressed", Toast.LENGTH_SHORT);
+                toast.show();   //TODO: Import der JSON-Datei hier implementieren
+            }
+        });
+        cancelButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Toast toast = Toast.makeText(getApplicationContext(), "Cancel Button pressed", Toast.LENGTH_SHORT);
+                toast.show();
+                finish();
+            }
+        });
+        durchsuchenButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Toast toast = Toast.makeText(getApplicationContext(), "Durchsuchen Button pressed", Toast.LENGTH_SHORT);
+                toast.show();   //TODO: Durchsuchen-Funktionen hier implementieren
+            }
+        });
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
+        public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_location_export, menu);
+        getMenuInflater().inflate(R.menu.menu_location_import, menu);
         return true;
     }
 
