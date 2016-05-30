eef5fd2578941b0830e91c791e563885989bdc93
pflingstring <nzaporojan@gmail.com>
2016-05-31 01:46:56 +0200

    add filter ui

--- SWP16_Impl_NST/app/src/main/java/swp/swp16_impl_nst/locations/views/LocationFilterActivity.java
+++ SWP16_Impl_NST/app/src/main/java/swp/swp16_impl_nst/locations/views/LocationFilterActivity.java
@@ -1,17 +1,39 @@
 package swp.swp16_impl_nst.locations.views;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 import swp.swp16_impl_nst.R;
+import swp.swp16_impl_nst.locations.LocationProvider;
+import swp.swp16_impl_nst.locations.LocationsAdapter;
 
 public class LocationFilterActivity extends AppCompatActivity
 {
+    private RecyclerView recyclerView;
+    private LocationsAdapter adapter;
+    private RecyclerView.LayoutManager layoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_location_filter);
+
+        // setup toolbar
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null)
+        {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setTitle("Filter");
+        }
+
+        recyclerView = (RecyclerView) findViewById(R.id.rview_filter_locations);
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+        adapter = new LocationsAdapter(LocationProvider.getLocationsCopy());
+        recyclerView.setAdapter(adapter);
     }
 }
