e91dfe2610459d05aae7a1883ce5d26f0b72d8f8
pflingstring <nzaporojan@gmail.com>
2016-05-31 02:10:42 +0200

    add click listener for filter buttons

--- SWP16_Impl_NST/app/src/main/java/swp/swp16_impl_nst/locations/views/LocationFilterActivity.java
+++ SWP16_Impl_NST/app/src/main/java/swp/swp16_impl_nst/locations/views/LocationFilterActivity.java
@@ -5,16 +5,22 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+import java.util.List;
 
 import swp.swp16_impl_nst.R;
 import swp.swp16_impl_nst.locations.LocationProvider;
 import swp.swp16_impl_nst.locations.LocationsAdapter;
+import swp.swp16_impl_nst.locations.model.filters.DistanceFilter;
+import swp.swp16_impl_nst.locations.model.filters.Predicate;
 
 public class LocationFilterActivity extends AppCompatActivity
 {
     private RecyclerView recyclerView;
     private LocationsAdapter adapter;
     private RecyclerView.LayoutManager layoutManager;
+    private List<Predicate> predicateList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -35,5 +41,51 @@ public class LocationFilterActivity extends AppCompatActivity
         recyclerView.setLayoutManager(layoutManager);
         adapter = new LocationsAdapter(LocationProvider.getLocationsCopy());
         recyclerView.setAdapter(adapter);
+    }
+
+    // filter buttons
+    public void distance_button(View view)
+    {
+        predicateList.add(new DistanceFilter());
+    }
+
+    public void string_button(View view)
+    {
+
+    }
+
+    public void rating_button(View view)
+    {
+
+    }
+
+    public void lastEditOn_button(View view)
+    {
+
+    }
+
+    public void createdOn_button(View view)
+    {
+
+    }
+
+    public void city_button(View view)
+    {
+
+    }
+
+    public void country_button(View view)
+    {
+
+    }
+
+    public void categories_button(View view)
+    {
+
+    }
+
+    public void owner_button(View view)
+    {
+
     }
 }
