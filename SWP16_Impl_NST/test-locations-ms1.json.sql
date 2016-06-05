7a4a7aa85128c55e212e55ccc871d4c94b650df0
Simon <rommelsp@students.uni-marburg.de>
2016-06-05 19:43:27 +0200

    RecyclerView for categorys implented

---
+++ SWP16_Impl_NST/test-locations-ms1.json.sql
@@ -0,0 +1,168 @@
+{
+  "locations": [
+    {
+      "address": {
+        "city": "Marburg",
+        "country": "Deutschland",
+        "number": "10",
+        "street": "Biegenstraße",
+        "zip": "35032"
+      },
+      "categories": [
+        {
+          "icon": 0,
+          "name": "Universität"
+        }
+      ],
+      "comment": "Verwaltung der Universität Marburg",
+      "contactDetails": {
+        "email": "info@uni-marburg.de",
+        "telephone": "+49 6421 2820",
+        "web": "http://www.uni-marburg.de"
+      },
+      "coordinates": {
+        "lat": 50.8102,
+        "lon": 8.774
+      },
+      "createdTimestamp": 1460486069617,
+      "id": "e67a8e9d-a1a1-4142-adae-22356d524a6c",
+      "lastModifiedTimestamp": 1460486069617,
+      "name": "Philipps-Universität Marburg",
+      "owner": {
+        "id": "1688ba16-c269-42ed-bb82-4c0ac95bb81c",
+        "name": "Felix"
+      },
+      "rating": "RATING_5"
+    },
+    {
+      "categories": [
+        {
+          "icon": 2,
+          "name": "Park"
+        }
+      ],
+      "comment": "Ein schöner Park",
+      "coordinates": {
+        "lat": 50.8029,
+        "lon": 8.7637
+      },
+      "createdTimestamp": 1460486069617,
+      "id": "d3c41ad1-0bcc-449e-be71-8e21eaf24923",
+      "lastModifiedTimestamp": 1460486069617,
+      "name": "Friedrichsplatz",
+      "owner": {
+        "id": "1688ba16-c269-42ed-bb82-4c0ac95bb81c",
+        "name": "Felix"
+      },
+      "rating": "RATING_5"
+    },
+    {
+      "categories": [
+        {
+          "icon": 1,
+          "name": "Bahnhof"
+        }
+      ],
+      "comment": "Hauptbahnhof in Marburg",
+      "contactDetails": {
+        "web": "http://www.bahn.de"
+      },
+      "coordinates": {
+        "lat": 50.8197,
+        "lon": 8.7748
+      },
+      "createdTimestamp": 1460486069617,
+      "id": "53e9e197-c5eb-4229-956a-bef04fa739ba",
+      "lastModifiedTimestamp": 1460486069617,
+      "name": "Marburg Hauptbahnhof",
+      "owner": {
+        "id": "1688ba16-c269-42ed-bb82-4c0ac95bb81c",
+        "name": "Felix"
+      },
+      "rating": "RATING_0"
+    },
+    {
+      "categories": [
+        {
+          "icon": 1,
+          "name": "Bahnhof"
+        }
+      ],
+      "comment": "Südbahnhof in Marburg",
+      "contactDetails": {
+        "web": "http://www.bahn.de"
+      },
+      "coordinates": {
+        "lat": 50.7939,
+        "lon": 8.7624
+      },
+      "createdTimestamp": 1460486069617,
+      "id": "6756d552-33db-4570-829d-ddaa354c2d74",
+      "lastModifiedTimestamp": 1460486069617,
+      "name": "Marburg Südbahnhof",
+      "owner": {
+        "id": "1688ba16-c269-42ed-bb82-4c0ac95bb81c",
+        "name": "Felix"
+      },
+      "rating": "RATING_1"
+    },
+    {
+      "categories": [
+        {
+          "icon": 0,
+          "name": "Universität"
+        },
+        {
+          "icon": 2,
+          "name": "Park"
+        }
+      ],
+      "comment": "Botanischer Garten auf den Lahnbergen",
+      "coordinates": {
+        "lat": 50.8021,
+        "lon": 8.8089
+      },
+      "createdTimestamp": 1460486069617,
+      "id": "c631a69b-254b-4c16-9a19-4d12ae0740fb",
+      "lastModifiedTimestamp": 1460486069617,
+      "mediaUrl": "https://www.uni-marburg.de/botgart/bilder/neuer_garten/gewaechshaeuser/farnhaus/image",
+      "name": "Neuer Botanischer Garten",
+      "owner": {
+        "id": "1688ba16-c269-42ed-bb82-4c0ac95bb81c",
+        "name": "Felix"
+      },
+      "rating": "RATING_4"
+    },
+    {
+      "address": {
+        "city": "Marburg",
+        "country": "Deutschland",
+        "street": "Hans-Meerwein-Straße",
+        "zip": "35032"
+      },
+      "categories": [
+        {
+          "icon": 0,
+          "name": "Universität"
+        }
+      ],
+      "comment": "Fachbereich 12 der Universität Marburg",
+      "contactDetails": {
+        "web": "http://www.uni-marburg.de/fb12"
+      },
+      "coordinates": {
+        "lat": 50.809,
+        "lon": 8.8103
+      },
+      "createdTimestamp": 1460465909616,
+      "id": "aa714bba-91d0-45cc-9157-00c22dd6809e",
+      "lastModifiedTimestamp": 1460486069616,
+      "name": "Fachbereich 12",
+      "owner": {
+        "id": "1688ba16-c269-42ed-bb82-4c0ac95bb81c",
+        "name": "Felix"
+      },
+      "rating": "NO_RATING"
+    }
+  ]
+}
